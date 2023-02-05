c02a786ed29ac1aedacc35a982e504a971a391be
Khashayar Nourian <knourianchali@gmail.com>
2023-02-05 07:06:53 +0330

    Annalyn's Infiltration solution

--- annalyns-infiltration/AnnalynsInfiltration.fs
+++ annalyns-infiltration/AnnalynsInfiltration.fs
@@ -1,12 +1,13 @@
 module AnnalynsInfiltration
 
-let canFastAttack (knightIsAwake: bool): bool = failwith "Please implement the 'canFastAttack' function"
+let canFastAttack (knightIsAwake: bool): bool= not knightIsAwake
 
 let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSpy' function"
+    knightIsAwake || archerIsAwake || prisonerIsAwake
 
 let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSignalPrisoner' function"
+    (not archerIsAwake) && prisonerIsAwake
 
 let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool): bool =
-    failwith "Please implement the 'canFreePrisoner' function"
+    (petDogIsPresent && not archerIsAwake) ||
+    (prisonerIsAwake && not archerIsAwake && not knightIsAwake)
