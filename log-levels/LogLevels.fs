91065156512f76b5d16751146c77783a5a325e03
Khashayar Nourian <knourianchali@gmail.com>
2023-02-06 06:53:24 +0330

    LogLevels Solution

--- log-levels/LogLevels.fs
+++ log-levels/LogLevels.fs
@@ -1,7 +1,13 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+let message (logLine: string): string = logLine.Substring(logLine.IndexOf(':')+1).Trim()
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let logLevel(logLine: string): string =
+    let fromIndex = logLine.IndexOf('[')+1
+    let toIndex = logLine.IndexOf(']')
+    logLine.Substring(fromIndex,toIndex-fromIndex).ToLower()
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let reformat(logLine: string): string = 
+    let logMessage = message logLine
+    let level = logLevel logLine
+    sprintf "%s (%s)" logMessage level
