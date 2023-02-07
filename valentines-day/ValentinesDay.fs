0ed266e20d79b61a7df014ccf2357eaa1998a292
Khashayar Nourian <knourianchali@gmail.com>
2023-02-07 14:58:54 +0330

    Valentine's Day Solution

--- valentines-day/ValentinesDay.fs
+++ valentines-day/ValentinesDay.fs
@@ -1,11 +1,35 @@
 module ValentinesDay
 
-// TODO: please define the 'Approval' discriminated union type
+type Approval = 
+    | Yes
+    | No
+    | Maybe
 
-// TODO: please define the 'Cuisine' discriminated union type
+type Cuisine = 
+    | Korean
+    | Turkish
 
-// TODO: please define the 'Genre' discriminated union type
+type Genre = 
+    | Crime
+    | Horror
+    | Romance
+    | Thriller
 
-// TODO: please define the 'Activity' discriminated union type
+type Activity =
+    | BoardGame
+    | Chill
+    | Movie of Genre
+    | Restaurant of Cuisine
+    | Walk of int
 
-let rateActivity (activity: Activity): Approval = failwith "Please implement the 'rateActivity' function"
+let rateActivity (activity: Activity): Approval = 
+    match activity with
+    | Activity.BoardGame -> Approval.No
+    | Activity.Chill -> Approval.No
+    | Activity.Movie Genre.Romance -> Approval.Yes
+    | Activity.Movie _ ->Approval.No
+    | Activity.Restaurant Cuisine.Korean -> Approval.Yes
+    | Activity.Restaurant Cuisine.Turkish -> Approval.Maybe
+    | Activity.Walk i when i<3 -> Approval.Yes
+    | Activity.Walk i when i <5 && i>=3 -> Approval.Maybe
+    | Activity.Walk _ -> Approval.No
