da8063336384bf044949717a058b53adceda5755
Khashayar Nourian <knourianchali@gmail.com>
2023-02-13 10:29:59 +0330

    Guessing game solution

--- guessing-game/GuessingGame.fs
+++ guessing-game/GuessingGame.fs
@@ -1,3 +1,9 @@
 module GuessingGame
 
-let reply (guess: int): string = failwith "Please implement the 'reply' function"
+let reply (guess: int): string = 
+    match guess with
+    | 42 -> "Correct"
+    | i when i = 41 || i = 43 -> "So close"
+    | i when i < 41 -> "Too low"
+    | i when i > 43 -> "Too high"
+    | _ -> ""
