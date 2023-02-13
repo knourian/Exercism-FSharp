6d6d621c8fb4808a2f8bcf6d13c8ce578b89acdf
Khashayar Nourian <knourianchali@gmail.com>
2023-02-13 15:22:50 +0330

    Pizza Pricing solution

--- pizza-pricing/PizzaPricing.fs
+++ pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,30 @@
 module PizzaPricing
+type Pizza = 
+    | Margherita 
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-// TODO: please define the 'Pizza' discriminated union type
+let pizzaPrice (pizza: Pizza): int = 
+    let rec calculate pizza total =
+        match pizza with
+        | Margherita -> 7 + total
+        | Caprese -> 9 + total
+        | Formaggio -> 10 + total
+        | ExtraSauce p -> calculate p (1 + total)
+        | ExtraToppings p -> calculate p (2 + total)
+    calculate pizza 0
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
-
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let orderPrice(pizzas: Pizza list): int = 
+    let rec calcPrice list total = 
+        match list with
+        | [] -> total
+        | head :: tail -> calcPrice tail (total + (pizzaPrice head))
+    
+    let price = calcPrice pizzas 0
+    
+    match List.length pizzas  with
+        | 1 -> 3 + price
+        | 2 -> 2 + price
+        | _ -> price
