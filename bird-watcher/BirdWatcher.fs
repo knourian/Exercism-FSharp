badd0dc137cbfeeb630908c50b02a4f124027cf7
Khashayar Nourian <knourianchali@gmail.com>
2023-02-21 12:55:36 +0330

    first try BirdWatcher

--- bird-watcher/BirdWatcher.fs
+++ bird-watcher/BirdWatcher.fs
@@ -1,19 +1,27 @@
 module BirdWatcher
 
-let lastWeek: int[] =
-   failwith "Please implement the 'lastWeek' value"
+let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
 
 let yesterday(counts: int[]): int =
-  failwith "Please implement the 'yesterday' function"
+  let length = counts |> Array.length
+  let index = length - 2
+  counts|> Array.item index
 
 let total(counts: int[]): int =
-  failwith "Please implement the 'total' function"
+  counts|> Array.sum
 
 let dayWithoutBirds(counts: int[]): bool =
-  failwith "Please implement the 'dayWithoutBirds' function"
+  counts |> Array.exists (fun elem -> elem = 0)
 
 let incrementTodaysCount(counts: int[]): int[] =
- failwith "Please implement the 'incrementTodaysCount' function"
+  let last = (counts |> Seq.last) + 1
+  let index = (counts |> Seq.length)  - 1
+  counts|> Array.updateAt index  last
 
 let oddWeek(counts: int[]): bool =
-  failwith "Please implement the 'oddWeek' function"
+  let odd = counts |> Array.filter (fun elm  -> elm % 2 = 0)
+  let even = counts |> Array.filter (fun elm -> elm % 2 <> 0)
+  let evenAllZero = even |> Array.forall (fun elm -> elm = 0)
+  let evenAllTen = even |> Array.forall (fun elm -> elm = 10)
+  let oddAllFive = odd |> Array.forall (fun elm -> elm = 5)
+  evenAllTen || evenAllZero || oddAllFive
