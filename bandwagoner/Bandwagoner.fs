905bde4628aaab5cd782226bb11252aae2962a30
Khashayar Nourian <knourianchali@gmail.com>
2023-02-28 12:16:14 +0330

    Bandwagoner solution

--- bandwagoner/Bandwagoner.fs
+++ bandwagoner/Bandwagoner.fs
@@ -1,25 +1,47 @@
 module Bandwagoner
 
-// TODO: please define the 'Coach' record type
+type Coach = 
+   {
+      Name : string
+      FormerPlayer : bool
+   }
 
-// TODO: please define the 'Stats' record type
+type Stats = 
+   {
+      Wins : int
+      Losses : int
+   }
 
-// TODO: please define the 'Team' record type
+type Team = 
+   {
+      Name : string
+      Coach : Coach
+      Stats : Stats
+   }
 
 let createCoach (name: string) (formerPlayer: bool): Coach =
-    failwith "Please implement the 'createCoach' function"
+   let coach:  Coach = {Name = name ; FormerPlayer = formerPlayer}
+   coach
 
 let createStats(wins: int) (losses: int): Stats =
-   failwith "Please implement the 'createStats' function"
+   let stats : Stats = {Wins= wins;Losses = losses}
+   stats
 
 let createTeam(name: string) (coach: Coach)(stats: Stats): Team =
-  failwith "Please implement the 'createTeam' function"
+  let team : Team = {Name = name;Coach = coach ; Stats = stats}
+  team 
 
 let replaceCoach(team: Team) (coach: Coach): Team =
-   failwith "Please implement the 'replaceCoach' function"
+   let newTeamCoach: Team = { team with Coach = coach}
+   newTeamCoach
 
 let isSameTeam(homeTeam: Team) (awayTeam: Team): bool =
-   failwith "Please implement the 'isSameTeam' function"
+   homeTeam = awayTeam
 
 let rootForTeam(team: Team): bool =
-   failwith "Please implement the 'rootForTeam' function"
+   match team with
+   | {Coach = coach} when coach.Name.Equals("Gregg Popovich")  || coach.FormerPlayer = true -> true
+   | {Name = name} when name.Equals("Chicago Bulls") -> true
+   | {Stats = stats} when stats.Wins >= 60 || stats.Losses > stats.Wins -> true
+   | _ -> false
+
