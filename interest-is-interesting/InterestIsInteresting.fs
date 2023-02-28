63e0700d24af1c4d4b978b88b7edd23ee6a7b0c0
Khashayar Nourian <knourianchali@gmail.com>
2023-02-28 08:41:23 +0330

    Interest is Interesting solution

--- interest-is-interesting/InterestIsInteresting.fs
+++ interest-is-interesting/InterestIsInteresting.fs
@@ -1,13 +1,19 @@
 module InterestIsInteresting
 
 let interestRate (balance: decimal): single =
-    failwith "Please implement the 'interestRate' function"
+    match balance with
+    | x when x < 0m -> 3.213f
+    | x when x < 1000m -> 0.5f
+    | x when x < 5000m -> 1.621f
+    | _ -> 2.475f
 
 let interest (balance: decimal): decimal =
-   failwith "Please implement the 'interest' function"
+   balance * (decimal)(interestRate balance) / 100m
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   failwith "Please implement the 'annualBalanceUpdate' function"
+   balance + interest balance
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
-   failwith "Please implement the 'amountToDonate' function"
+   match balance with
+   | x when x <=0m -> 0
+   |_ -> (int)( (float)balance *  (taxFreePercentage / (float)100 * (float)2))
