03b0c60da16b099198f696dade6db61a9e52ecb1
Khashayar Nourian <knourianchali@gmail.com>
2023-03-04 08:30:57 +0330

    Hamming solution

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -1,3 +1,11 @@
 module Hamming
 
-let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
+let distance (strand1: string) (strand2: string): int option = 
+    if (strand1|> Seq.length)  <> (strand2 |> Seq.length) then None
+    else
+        let strand = strand1 |>Seq.zip strand2
+        strand 
+        |> Seq.filter (fun (elem1 ,elem2 ) -> elem1 <> elem2)
+        |> Seq.length
+        |> Some
+            
