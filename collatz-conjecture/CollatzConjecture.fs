2eb5e03e6d914f42e1e431bb4cc6f7b1f7cc53c7
Khashayar Nourian <knourianchali@gmail.com>
2023-03-12 13:09:21 +0330

    Collatz Conjecture Solution

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,15 @@
 module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let isEven(number : int) = number % 2 = 0
+
+let steps (number: int): int option = 
+    let rec compute(n:int) (steps:int) : int =
+        if( n = 1 ) then steps
+        else
+            if (isEven n) 
+                then compute (n/2) (steps + 1)
+            else 
+                compute (3 * n + 1) (steps + 1)
+    if number <= 0 then None
+    else 
+        Some (compute number 0)
