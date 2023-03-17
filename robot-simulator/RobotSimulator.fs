45c7d0b9ac5d18fee83d3ec5245227fd09b139b1
Khashayar Nourian <knourianchali@gmail.com>
2023-03-17 12:28:05 +0330

    Robot Simulator init

---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,8 @@
+module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+
+let create direction position = failwith "You need to implement this function."
+
+let move instructions robot = failwith "You need to implement this function."
