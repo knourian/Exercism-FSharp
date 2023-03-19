63e1af39ec6583964bcfaefec4dd7a590a7b741d
Khashayar Nourian <knourianchali@gmail.com>
2023-03-19 08:32:02 +0330

    Protein Translation solution

--- protein-translation/ProteinTranslation.fs
+++ protein-translation/ProteinTranslation.fs
@@ -1,3 +1,40 @@
 module ProteinTranslation
 
-let proteins rna = failwith "You need to implement this function."
+let codons (nucleotide:string) = 
+    match nucleotide with 
+    | "" -> ""
+    | "AUG" -> "Methionine"
+    | "UUU" -> "Phenylalanine"
+    | "UUC" -> "Phenylalanine"
+    | "UUA" -> "Leucine"
+    | "UUG" -> "Leucine"
+    | "UCU" -> "Serine"
+    | "UCC" -> "Serine"
+    | "UCA" -> "Serine" 
+    | "UCG" -> "Serine" 
+    | "UAU" -> "Tyrosine"
+    | "UAC" -> "Tyrosine"
+    | "UGU" -> "Cysteine"
+    | "UGC" -> "Cysteine"
+    | "UGG" -> "Tryptophan"
+    | "UAA" -> "Stop"
+    | "UAG" -> "Stop"
+    | "UGA" -> "Stop"
+    | _ -> ""
+
+
+
+
+
+let proteins( rna:string) = 
+    let rec next (remianRna : string) (result: string list)=
+        match remianRna with
+        | x when x.Length < 3 -> result
+        | _-> 
+            let head = remianRna.[0..2]
+            let cod = codons head
+            if cod = "Stop" then result
+            else 
+                let tail = remianRna.[3..]
+                next tail (result @ [cod])
+    next rna list.Empty
